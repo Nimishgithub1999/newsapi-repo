282c8afc04dd27fdede3bf7dcd63e6bf373bc438
Nimishgithub1999 <115951215+Nimishgithub1999@users.noreply.github.com>
2023-11-24 23:14:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,23 +7,20 @@ load_dotenv()
 
 app = Flask(__name__)
 
-
 apiKey = os.getenv('apikey')
-# Initialize News API client
 newsapi = NewsApiClient(api_key=apiKey)
 
 @app.route('/')
 def homepage():
-    # Example: Get top headlines without specifying sources
     top_headlines = newsapi.get_top_headlines(
         q='bitcoin',
         category='business',
         language='en',
         country='us'
     )
-
-    # Example: Get sources
     sources = newsapi.get_sources()
 
     return render_template("index.html", top_headlines=top_headlines, sources=sources)
 
+# Set the 'run' attribute as the Flask app instance
+run = app
